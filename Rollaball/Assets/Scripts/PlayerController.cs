21a9ab9eea1e35519ad2fcba8034aa87143039c2
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-03-21 22:22:07 -0300

    Tutorial finalizado

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+using TMPro;
+
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int count;
+
+    private int maxScore = 8;
 
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent <Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     private void FixedUpdate() {
@@ -26,5 +36,20 @@ public class PlayerController : MonoBehaviour {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count >= maxScore) {
+            winTextObject.SetActive(true);
+        }
     }
 }
